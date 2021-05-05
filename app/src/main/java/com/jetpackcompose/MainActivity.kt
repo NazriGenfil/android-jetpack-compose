cc3e53db4e11e01948f8c68f53c03ca3ddea8d3b
Nazri <abienazri23@gmail.com>
2021-05-06 00:59:33 +0700

    testing modifier

--- app/src/main/java/com/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/jetpackcompose/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -26,25 +29,30 @@ class MainActivity : ComponentActivity() {
                     .background(Color.Green)
                     .fillMaxHeight(0.5f)
                     .fillMaxWidth()
-                    .padding(top = 60.dp)
-//                    .requiredWidth(600.dp)
+                    .fillMaxSize()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
             ){
-                Text(text = "Hello")
-                Text(text = "World")
+                Text(text = "Hello", modifier = Modifier
+                    .padding(5.dp)
+                    .offset(20.dp, 20.dp)
+                    .border(5.dp, Color.Yellow)
+                    .padding(10.dp)
+                    .clickable {
+
+                    }
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(text = "World", modifier = Modifier
+                    .clickable {
+
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
     }
 }
